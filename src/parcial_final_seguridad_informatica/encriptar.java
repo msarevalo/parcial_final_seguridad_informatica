cba2e30a5e1a9243a6145fbcbe143a93b13064be
msarevalo <msarevalo@unbosque.edu.co>
2019-04-16 19:30:26 -0500

    validaciones de claves

--- src/parcial_final_seguridad_informatica/encriptar.java
+++ src/parcial_final_seguridad_informatica/encriptar.java
@@ -26,24 +26,56 @@ public class encriptar {
 		abc.add('x'); abc.add('y');
 		abc.add('z'); abc.add('_');
 		
-		String cadena = "lina";
+		String cadena = "anti";
 		cadena = cadena.replace(" ", "_");
 		cadena = cadena.toLowerCase();
 		double acumulado = 0;
 		double valor = 0;
+		double base = Math.pow(28, 4);
 		double a = 0;
 		double b = 0;
+		double ae = 0;
+		double be = 0;
+		int primero = 0;
+		int segundo = 0;
 		Scanner usera = new Scanner(System.in);
 		Scanner userb = new Scanner(System.in);
 		
-		System.out.println("Ingresar la primera clave: ");
-	    a = usera.nextDouble();
+		while(primero == 0) {
+			System.out.println("Ingresar la primera clave: ");
+			ae = usera.nextDouble();
+			if(ae>=307329) {
+				if(ae<=base) {
+					if(MCD_Euclides(ae, base)==1) {
+						a = ae;
+						primero++;
+					}else {
+						System.out.println("La clave ingresada no tiene primo relativo, por favor ingresala nuevamente");
+					}
+				}else {
+					System.out.println("La clave ingresada debe ser menor o igual a 614656, por favor ingresala nuevamente");
+				}
+			}else {
+				System.out.println("La clave ingresada debe ser mayor o igual a 307329, por favor ingresala nuevamente");
+			}
+		}
 	    
-	    System.out.println("Ingresar la segunda clave: ");
-	    b = userb.nextDouble();
+		while(segundo == 0) {
+			System.out.println("Ingresar la segunda clave: ");
+			be = userb.nextDouble();
+			if(be>=307329) {
+				if(be<=base) {
+					b = ae;
+					segundo++;
+				}else {
+					System.out.println("La clave ingresada debe ser menor o igual a 614656, por favor ingresala nuevamente");
+				}
+			}else {
+				System.out.println("La clave ingresada debe ser mayor o igual a 307329");
+			}
+		}
 
-		
-		double base = Math.pow(28, 4);
+		//System.out.println(MCD_Euclides(a, base));
 		//System.out.println(abc.indexOf('z'));
 		
 		for (int i=0; i<cadena.length(); i++) {
@@ -63,7 +95,7 @@ public class encriptar {
 			acumulado = acumulado + valor;
 		}
 		
-		System.out.println(acumulado);
+		//System.out.println(acumulado);
 		
 		double e = ((a*acumulado)+b)%base;
 
@@ -78,6 +110,15 @@ public class encriptar {
 		System.out.println(encriptado);
 		//System.out.println(abc.get(letra1) + " " + abc.get(letra2) + " " + abc.get(letra3) + " " + abc.get(letra4));
 
+	}
+	
+	private static double MCD_Euclides(double a, double b){
+	    while(b != 0){
+	         double t = b;
+	         b = a % b;
+	         a = t;
+	    }
+	    return a;
 	}
 
 }
